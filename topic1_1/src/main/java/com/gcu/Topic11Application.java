e5640d1c1b45ef6292a72d42bae9832a71a0cf2d
Yonas Habte <yonas377@gmail.com>
2025-11-14 22:56:11 -0500

    file upload

---
+++ topic1_1/src/main/java/com/gcu/Topic11Application.java
@@ -0,0 +1,13 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Topic11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Topic11Application.class, args);
+	}
+
+}
